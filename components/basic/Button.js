d9841fc2f6bf0828cb3f4dfb19fa841d0819f06e
dandingol03 <1291248710@qq.com>
2016-03-15 00:05:13 +0800

    too much file to be commited

--- components/basic/Button.js
+++ components/basic/Button.js
@@ -1,15 +1,12 @@
-/**
- * desc,Created by outstudio on 16/3/11.
- * @class ButtonElement
- */
+
 import React from 'react';
 
 var ButtonElement=React.createClass({
         /**
          * options,this property will store the configure which behaviour u want the ButtonElement component to behaviour
-         * @property options
-         * @type Object
-         * @returns {{options: *}}
+         * @method getInitialState
+         * @param options {Object} u configure in this object
+         * @returns the state of component
          */
         getInitialState:function(){
                 var options;
