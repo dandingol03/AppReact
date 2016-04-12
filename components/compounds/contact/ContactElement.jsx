8be929d9c7f4d666998f43538345fa68b4087055
dandingol03 <1291248710@qq.com>
2016-04-12 18:56:52 +0800

    add Contact component

---
+++ components/compounds/contact/ContactElement.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+
+/**
+ * Bootstrap
+ */
+var Contact=React.createClass({
+    commitCb:function(evt){
+
+    },
+    handleChange:function(evt){
+
+    },
+    getInitialState:function() {
+        //twSpan,代表通过计算获得的表宽,不设置即为2
+
+    }
+    ,
+    render:function() {
+    }
+})
+
+export default Contact
