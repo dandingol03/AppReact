02be8f1a886c92e7680bf5e94c68328e99210eea
dandingol03 <1291248710@qq.com>
2016-04-27 22:55:01 +0800

    repair OrdinaryTable component

---
+++ components/basic/Download.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+
+
+var Download=React.createClass({
+
+    render:function() {
+        var attach=null;
+        if(this.props.attachId!==undefined&&this.props.attachId!==null)
+        {
+            var href="gradms/attachment/attachmentDownloadAttachmentBSFile.do?attachId="+this.props.attachId;
+                attach=<a href="#" className="btn btn-block btn-primary " href={href}>Primary
+                    <span className="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></a>
+        }
+
+        return(<div>
+                {attach}
+                </div>);
+
+
+    }
+});
+export default Download;
