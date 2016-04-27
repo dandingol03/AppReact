76e1046960e8b60598498737c5673265beb253b5
dandingol03 <1291248710@qq.com>
2016-04-27 22:53:46 +0800

    repair OrdinaryTable component

---
+++ components/basic/DropDown.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+
+
+var DropDown=React.createClass({
+    getInitialState:function(){
+
+
+    },
+    render:function(){
+
+
+
+
+    }
+});
+export default DropDown;
