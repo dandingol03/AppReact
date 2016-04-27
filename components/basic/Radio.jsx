b279030c9461fbaaf2c824b1b4b2e60fa9e221b9
dandingol03 <1291248710@qq.com>
2016-04-28 01:46:59 +0800

    append Radio component

---
+++ components/basic/Radio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+import dict from '../../data/json/dictionary.json';
+
+var Radio=React.createClass({
+    selectCb:function(){
+
+
+
+    },
+    render:function(){
+
+    }
+});
+export default Radio;
