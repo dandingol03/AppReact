dac6c80461a170216ec209f591661ec666c7df12
dandingol03 <1291248710@qq.com>
2016-04-28 14:41:29 +0800

    repair OrdinaryTable component

--- components/basic/LinkElement.jsx
+++ components/basic/LinkElement.jsx
@@ -4,17 +4,24 @@ import React from 'react';
 
 var LinkElement=React.createClass({
     clickCb:function(evt){
-        this.props.clickCb(evt);
+        if(this.props.linkCb!==undefined&&this.props.linkCb!==null)
+        {
+            console.log();
+            console.log();
+            console.log();
+            this.props.linkCb(evt);
+        }
+
     },
     render:function(){
         var data$index;
         if(this.props["data-index"]!==null&&this.props["data-index"]!==undefined)
-             data$index=this.props["data-index"];
+            data$index=this.props["data-index"];
 
         //link,上层组件传来的超链
         var link;
-        if(this.props.link!==undefined&&this.props.link!==null)
-            link=this.props.link;
+        if(this.props.to!==undefined&&this.props.to!==null)
+            link=this.props.to;
         else
             link="javascript:void(0)";
 
@@ -23,8 +30,15 @@ var LinkElement=React.createClass({
             alignStyle={
                 textAlign:this.props.align
             }
+
+
+        var query;
+        if(this.props["data-query"]!==undefined&&this.props["data-query"]!==null)
+        {
+            query=this.props["data-query"];
+        }
         return (<a href={link}  className={this.props.linkClass} data-index={data$index}
-                   onClick={this.clickCb} style={alignStyle}>
+                   onClick={this.clickCb} style={alignStyle} data-query={query}>
             {this.props.children}</a>)
     }
 });
