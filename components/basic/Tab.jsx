662b2fb6145900ff8f65d9ad1a0d7b59813f8ec2
guaneryuanyuan <zhengyuanyuan1994@163.com>
2016-05-20 09:31:07 +0800

    new tab.jsx

--- components/basic/Tab.jsx
+++ components/basic/Tab.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import {render} from 'react-dom';
+import '../../css/components/basic/tab.css';
+
 
 var Tab=React.createClass({
     getInitialState:function(){
+        var data;
+        if(this.props.data!==undefined&&this.props.data!==null){
+            data=this.props.data;
+        }
 
-
-
+        return({data:data});
+    },
+    linkCb:function(evt){
+        var target=evt.target;
+        var $target=$(target);
+        var index = $target.attr("data-index");
+        $target.addClass("active");
+        var lis=$($target.parent()).find("li");
+        for(var i=0;i<lis.length;i++) {
+            var $li=$(lis[i]);
+            if(i!==index) {
+                $li.removeClass("active");
+            }
+        }
+        this.setState({selected: index});
     },
     render:function(){
 
+        var lis=new Array();
+        var linkCb=this.linkCb;
+        this.props.data.map(function(item,i){
+                lis.push(<li className="cli" onClick={linkCb} key={i}>item.label</li>);
+        })
+
+        return(<div>
+                    <div>
+                          <ul>
+                              {lis}
+                          </ul>
+                    </div>
+               </div>)
 
 
 
