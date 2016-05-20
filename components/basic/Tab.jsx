af92a10eca7168914105661db6d23c32012d326f
guaneryuanyuan <zhengyuanyuan1994@163.com>
2016-05-20 08:46:14 +0800

    new

---
+++ components/basic/Tab.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+var Tab=React.createClass({
+    getInitialState:function(){
+
+
+
+    },
+    render:function(){
+
+
+
+
+    }
+});
+export default Tab;
