e0efd2adcb9b7ad7f177c4cfffd109199d2c26e6
dandingol03 <1291248710@qq.com>
2016-05-20 16:26:28 +0800

    add CustomMenu components

--- entrys/201513569/graduate/serviceHobby/customMenu/customMenuMain.jsx
+++ entrys/201513569/graduate/serviceHobby/customMenu/customMenuMain.jsx
@@ -10,11 +10,10 @@ function Boot() {
     var query = {
         url   : "/bsuims/reactPageDataRequest.do",
         params: {
-            reactPageName  : "degreeAnonymousThesisReviewResult",
-            reactActionName: "deegreeThesisReviewResultUseReact"
+            reactPageName  : "",
+            reactActionName: ""
         }
     }
-
     var data = [
         {selected: true, name: "a"},
         {selected: true, name: "b"},
@@ -25,15 +24,34 @@ function Boot() {
         {selected: false, name: "g"},
         {selected: false, name: "h"},
         {selected: false, name: "i"},
-    ]
+        {selected: false, name: "j"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"},
+        {selected: false, name: "k"}
+    ];
+    require(["../../../../../components/basic/CustomMenu.jsx"], function (Component) {
+        render(
+            React.createElement(Component, {data: data, query: query})
+            , document.getElementById('root'));
+
+    });
 
 
-    render(
-        <CustomMenu
-            data={data}
-            query={query}
-            />
-        , document.getElementById('root'))
+    //render(
+    //    <CustomMenu
+    //        data={data}
+    //        query={query}
+    //        />
+    //    , document.getElementById('root'));
 
 
 }
