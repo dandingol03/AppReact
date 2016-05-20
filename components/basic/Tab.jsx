73bd5ac3874307fb5bc40c8822b02e1155e77330
guaneryuanyuan <zhengyuanyuan1994@163.com>
2016-05-20 13:57:32 +0800

    update tab.jsx

--- components/basic/Tab.jsx
+++ components/basic/Tab.jsx
@@ -4,38 +4,36 @@ import '../../css/components/basic/tab.css';
 
 
 var Tab=React.createClass({
-    getInitialState:function(){
-        var data;
-        if(this.props.data!==undefined&&this.props.data!==null){
-            data=this.props.data;
-        }
-
-        return({data:data});
-    },
     linkCb:function(evt){
         var target=evt.target;
         var $target=$(target);
         var index = $target.attr("data-index");
-        $target.addClass("active");
-        var lis=$($target.parent()).find("li");
-        for(var i=0;i<lis.length;i++) {
-            var $li=$(lis[i]);
-            if(i!==index) {
-                $li.removeClass("active");
-            }
-        }
+
         this.setState({selected: index});
+    },
+    getInitialState:function(){
+
+        return ({selected: -1});
     },
     render:function(){
 
         var lis=new Array();
         var linkCb=this.linkCb;
+        var state=this.state;
         this.props.data.map(function(item,i){
-                lis.push(<li className="cli" onClick={linkCb} key={i}>item.label</li>);
-        })
+            if(state.selected!==undefined&&state.selected!==null&&
+                i==parseInt(state.selected))
+            {
+                lis.push(<li className="cli active" onClick={linkCb} key={i} data-index={i}>{item.name}</li>);
+            }
+            else{
+                lis.push(<li className="cli" onClick={linkCb} key={i} data-index={i}>{item.name}</li>);
+            }
 
-        return(<div>
-                    <div>
+        });
+
+        return(<div className="tab">
+                    <div className="tab-body" >
                           <ul>
                               {lis}
                           </ul>
