bd415f628ffef84211a59d26577c216fc3e37d8a
guaneryuanyuan <zhengyuanyuan1994@163.com>
2016-06-14 10:48:38 +0800

    bell

--- components/basic/Nav.jsx
+++ components/basic/Nav.jsx
@@ -24,6 +24,9 @@ var Nav=React.createClass({
         } else {
             App.unSwing("#bell");
         }
+        this.setState({fieldCount:fieldCount});
+
+
         //for (var id in stores) {
         //    console.log("id=" + stores[id].route);
         //    console.log("data=" + stores[id].data);
@@ -140,8 +143,8 @@ var Nav=React.createClass({
         var auto;
         if(this.props.auto===true||this.props.auto==="true")
             auto=true;
-
-        return ({data:data,data$initialed:data$initialed,auto:auto});
+        var fieldCount=0;
+        return ({data:data,data$initialed:data$initialed,auto:auto,fieldCount:fieldCount});
     },
     render:function(){
         if(this.state.data$initialed!==true&&(this.props.data==null||this.props.data==undefined))
@@ -318,7 +321,9 @@ var Nav=React.createClass({
                         <div className="center">
                             <ul className="link">
                                 <li><a id="bell" href="javascript:void(0)" className="fa fa-bell-o"
-                                       style={{marginTop:"10px"}}></a></li>
+                                       style={{marginTop:"10px"}}><span style={{color:"#f00", paddingLeft:"3px"}}>{this.state.fieldCount}</span></a>
+
+                                </li>
                                 <li><a href="javascript:void(0)">刷新</a></li>
                                 <em className="global-top-item global-top-seperator">|</em>
                                 <li><a href="javascript:void(0)">退出</a></li>
