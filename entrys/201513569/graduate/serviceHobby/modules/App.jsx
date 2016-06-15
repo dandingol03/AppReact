3d379ff5cde7af0dcb735a43093290979f78838e
guaneryuanyuan <zhengyuanyuan1994@163.com>
2016-06-15 09:57:50 +0800

    add bg in header

--- entrys/201513569/graduate/serviceHobby/modules/App.jsx
+++ entrys/201513569/graduate/serviceHobby/modules/App.jsx
@@ -27,7 +27,7 @@ var App =React.createClass({
          */
             <div>
                 <Nav logo={Deploy.getResourceDeployPrefix()+"/images/school_logo.png"} data={MENU}/>
-
+                <div className="topbg"></div>
 
                 <div className="keyNavigation">
                     <div className="top">
