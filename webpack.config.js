9a253f3e9bfe9d3bcc865bb7e59c399c63c0ab39
dandingol03 <1291248710@qq.com>
2016-06-17 09:44:13 +0800

    add config temporarily

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        path.resolve(__dirname, './entrys/201513569/graduate/serviceHobby/index.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: "./build",
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        port:3000,
+        hot:true,
+        proxy:{
+            '/serviceHall/*.do':{
+                    target: 'http://localhost:8090/',
+                    secure: false
+            }
+        }
+    },
+    plugins: [
+       new webpack.HotModuleReplacementPlugin()
+    ],module: {
+
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                loader: 'jsx-loader?harmony'
+            },
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.less$/, loader: "style!css!less" },
+            {test:/\.json$/,loader:"json"},
+            {
+                test: /\.jsx?$/,
+                loader:'babel',
+                exclude:'/node_modules/',
+                query: {
+                    presets: ['es2015','react']
+                }
+            },
+            {test: /\.png$/, loader: "url-loader?mimetype=image/png"},
+            {test: /\.gif$/, loader: "url-loader?mimetype=image/gif"},
+            {test: /\.jpg$/, loader: "url-loader?mimetype=image/jpeg"}
+        ]
+    }
+};
